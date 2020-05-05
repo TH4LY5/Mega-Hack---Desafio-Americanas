cf2614fb465fe53cfa212832ad52a16582f8e3ff
Camila Alcantara <camilamariana2008@hotmail.com>
2020-05-04 23:29:32 -0300

    inicio

--- Americanas APP/src/app/tab1/tab1.page.ts
+++ Americanas APP/src/app/tab1/tab1.page.ts
@@ -9,19 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Tab1Page {
   public tipoComercio: any;
+  public tipoItem: any;
   constructor(public navCtrl: NavController,
     private route: ActivatedRoute) {
     this.tipoComercios();
+    this.tipoItens();
   }
 
   tipoComercios() {
 
     this.tipoComercio = [
-      { id: 1, nome: 'Supermercado', img: 'supermercado.jpg', descricao: '"Aqui você encontra todos os supermercados"' },
-      { id: 2, nome: 'Loja', img: 'loja.jpg', descricao: '"Aqui você encontra todas as lojas"' },
-      { id: 3, nome: 'Lanchonete', img: 'lanchonete.jpg', descricao: '"Aqui você encontra todas as lanchonetes"' },
-      { id: 4, nome: 'Restaurante', img: 'resturante.jpg', descricao: '"Aqui você encontra todos os restaurantes"' },
-      { id: 5, nome: 'Farmacia', img: 'farmacia.jpg', descricao: '"Aqui você encontra todos as farmacias"' }
+      { id: 1,  img: 'iphone.png'},
+      // { id: 2,  img: 'iphone.png' },
+      // { id: 3, nome: 'Lanchonete', img: 'lanchonete.jpg', descricao: '"Aqui você encontra todas as lanchonetes"' },
+      // { id: 4, nome: 'Restaurante', img: 'resturante.jpg', descricao: '"Aqui você encontra todos os restaurantes"' },
+      // { id: 5, nome: 'Farmacia', img: 'farmacia.jpg', descricao: '"Aqui você encontra todos as farmacias"' }
+    ]
+  }
+
+  tipoItens(){
+    this.tipoItem = [
+      { id: 1, nome: 'Kit kat', img: 'kitkat.jpg', descricao: '"Chocolate Kitkat 4 Fingers Ao Leite 41,5g"' },
+      { id: 2, nome: 'Prestigio Branco', img: 'prestigioB.jpg', descricao: '"Chocolate Prestigio Branco Nestlé 114g 6 Unidades' },
+      { id: 3, nome: 'Hersheys', img: 'hersheys.jpg', descricao: '"Tablete De Chocolate Ao Leite Com Ovomaltine 87g - Hersheys"' }
     ]
   }
 }
