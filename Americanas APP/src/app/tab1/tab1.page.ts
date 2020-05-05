a6df4aa15558dfe3ce6a25e7fd81205e1a664ef6
TH4LY5 <30905866+TH4LY5@users.noreply.github.com>
2020-05-04 21:54:38 -0300

    CARDS

--- Americanas APP/src/app/tab1/tab1.page.ts
+++ Americanas APP/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public tipoComercio: any;
+  constructor(public navCtrl: NavController,
+    private route: ActivatedRoute) {
+    this.tipoComercios();
+  }
 
-  constructor() {}
+  tipoComercios() {
 
+    this.tipoComercio = [
+      { id: 1, nome: 'Supermercado', img: 'supermercado.jpg', descricao: '"Aqui você encontra todos os supermercados"' },
+      { id: 2, nome: 'Loja', img: 'loja.jpg', descricao: '"Aqui você encontra todas as lojas"' },
+      { id: 3, nome: 'Lanchonete', img: 'lanchonete.jpg', descricao: '"Aqui você encontra todas as lanchonetes"' },
+      { id: 4, nome: 'Restaurante', img: 'resturante.jpg', descricao: '"Aqui você encontra todos os restaurantes"' },
+      { id: 5, nome: 'Farmacia', img: 'farmacia.jpg', descricao: '"Aqui você encontra todos as farmacias"' }
+    ]
+  }
 }
